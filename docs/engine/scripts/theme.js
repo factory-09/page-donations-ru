25c3bb90b9ffeec359bae884bdde7de5c62e190d
Kitsune Solar <kitsune.solar@gmail.com>
2018-12-19 13:06:00 +0300

    2018-12-19 10:06:00

---
+++ docs/engine/scripts/theme.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Loading functions.
+ * ---------------------------------------------------------------------------------------------------------------------
+ */
+
+$(function () {
+	extJS_paymentTarget('storage/target.json');
+	extJS_clipBoard();
+	extJS_fieldValidation();
+});
